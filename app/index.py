3bc4769da0f86ad4db9ab733396897038ead6ef4
Sekai <50212311+ig4e@users.noreply.github.com>
2021-11-08 19:23:01 +0200

    Add files via upload

---
+++ app/index.py
@@ -0,0 +1,23 @@
+import cloudscraper
+from flask import Flask
+from flask import request
+
+app = Flask(__name__) #sets the "app" variable to a Flask instance
+scraper = cloudscraper.CloudScraper()
+
+
+@app.route('/')
+def mainR():
+    return "OK"
+
+
+@app.route('/json-example', methods=['POST'])
+def json_example():
+    request_data = request.get_json()
+    print(request_data['url'])
+    if request_data['get']:
+      return scraper.get(request_data['url']).text
+    else:
+      headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+      return scraper.post(request_data['url'], headers=headers, data=request_data['data']).text
+  
