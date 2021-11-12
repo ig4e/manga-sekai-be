943c752de38ae5260d42cac18a1e7715a306c6f2
Sekai <50212311+ig4e@users.noreply.github.com>
2021-11-12 03:27:40 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -15,8 +15,9 @@ def mainR():
 def json_example():
     request_data = request.get_json()
     print(request_data['url'])
+    cookies = {'cf_clearance ': '04YmwuvvkkTSry5Ojo2Eu0Ci9eSOo9KiwxBeLTbR2U8-1636680056-0-150'}
     if request_data['get']:
-      return scraper.get(request_data['url']).text
+      return scraper.get(request_data['url'] cookies=cookies).text
     else:
       headers = {'Content-Type': 'application/x-www-form-urlencoded'}
       return scraper.post(request_data['url'], headers=headers, data=request_data['data']).text
